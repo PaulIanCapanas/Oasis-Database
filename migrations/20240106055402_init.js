ca14b039dc915a1679b0e04a278c5ce12418f871
Arshie13 <aaron.ciervo-20@cpu.edu.ph>
2024-01-06 13:58:50 +0800

    minor changes

---
+++ migrations/20240106055402_init.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('User', table => {
+    table.integer('list_of_buildings').nullable();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('User', table => {
+    table.dropColumn('list_of_buildings');
+  })
+};
